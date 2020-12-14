3d9ef03847ee0e1fde6ab51a981e5954bbcd6e80
tamara96mc <tamara.96mc@gmail.com>
2020-12-14 14:20:20 +0100

    Updates

---
+++ precache-manifest.611ece7d7383ea845afebbea35ac3694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872df790582fcce9efe1d9396b7c099a",
+    "url": "/noticias/index.html"
+  },
+  {
+    "revision": "a56940c6a2e90906e148",
+    "url": "/noticias/static/css/main.473b38e5.chunk.css"
+  },
+  {
+    "revision": "f712c33682c5179bbeac",
+    "url": "/noticias/static/js/2.6e379b88.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/noticias/static/js/2.6e379b88.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a56940c6a2e90906e148",
+    "url": "/noticias/static/js/main.db1744b6.chunk.js"
+  },
+  {
+    "revision": "39ba9c50c664324683c6",
+    "url": "/noticias/static/js/runtime-main.fae7e0df.js"
+  }
+]);
